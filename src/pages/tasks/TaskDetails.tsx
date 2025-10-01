0cf505840a584ae83d159a1220444495789b6e57
Pramod <krishna.pramod2@gmail.com>
2025-10-01 16:09:31 +0530

    Completed the task detail screen

--- src/pages/tasks/TaskDetails.tsx
+++ src/pages/tasks/TaskDetails.tsx
@@ -1,13 +1,8 @@
 import StatusBadge from "@/components/shared/StatusBadge";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea";
+import { projects } from "@/data/project";
 import { tasks } from "@/data/task";
 import { dateFormatter } from "@/lib/utils";
 import { FilePenLine, Trash } from "lucide-react";
@@ -19,18 +14,20 @@ export const loader = ({ params }: LoaderFunctionArgs) => {
       task.projectId === Number(params.projectId) &&
       task.id === Number(params.taskId)
   );
-  console.log(task);
-  return task;
+  const project = projects.find(
+    (project) => project.id === Number(params.projectId)
+  );
+  return { project, task };
 };
 
 const TaskDetails = () => {
-  const taskData = useLoaderData<ReturnType<typeof loader>>();
+  const { project, task } = useLoaderData<ReturnType<typeof loader>>();
   return (
     <div className="p-8 space-y-6">
       <div className="flex items-center justify-between pb-8">
         <div className="flex items-end gap-2 m-0">
-          <h1 className="font-bold text-5xl">{taskData?.title}</h1>
-          <StatusBadge status={taskData?.status || "unknown"} />
+          <h1 className="font-bold text-5xl">{task?.title}</h1>
+          <StatusBadge status={task?.status || "unknown"} />
         </div>
         <div className="flex gap-2">
           <Button variant="default">
@@ -45,30 +42,44 @@ const TaskDetails = () => {
       </div>
       <Card className="gap-0">
         <CardHeader>
-          <CardTitle className="font-semibold text-xl">Details</CardTitle>
-          <CardDescription>{taskData?.description}</CardDescription>
+          <CardTitle className="font-semibold text-xl">Task Details</CardTitle>
         </CardHeader>
         <CardContent className="space-y-2 grid grid-cols-3">
-          <div className="mt-6">
-            <p className="text-muted-foreground">Due Date</p>
-            {taskData?.dueDate ? dateFormatter(taskData.dueDate) : "N/A"}
-          </div>
-          <div className="mt-6">
-            <p className="text-muted-foreground">Assignee</p>
-            <p>{taskData?.assignee}</p>
-          </div>
-          <div className="mt-6">
-            <p className="text-muted-foreground">Priority</p>
-            <p>{taskData?.priority}</p>
-          </div>
-          <div className="mt-6">
-            <p className="text-muted-foreground">Category</p>
-            <p>{taskData?.category}</p>
-          </div>
+          {task &&
+            (Object.keys(task) as Array<keyof typeof task>).map((key) => {
+              let data = task[key];
+              if (key === "dueDate") {
+                data = dateFormatter(data as string);
+              }
+              return (
+                <div className="mt-6">
+                  <p className="text-muted-foreground capitalize">
+                    {key.replace(/([A-Z])/g, " $1").trim()}
+                  </p>
+                  {data}
+                </div>
+              );
+            })}
         </CardContent>
-        <div className="mt-6 p-4">
-          <p>Comments</p>
-          <Input placeholder="Add a comment" />
+        <h2 className="px-6 pt-6 text-xl font-semibold">Project Details</h2>
+        <CardContent className="grid grid-cols-3">
+          {project &&
+            (Object.keys(project) as Array<keyof typeof project>).map((key) => {
+              let data = project[key];
+              if (key === "startDate" || key === "endDate") {
+                data = dateFormatter(data as string);
+              }
+              return (
+                <div className="mt-6">
+                  <p className="text-muted-foreground capitalize">{key}</p>
+                  {data}
+                </div>
+              );
+            })}
+        </CardContent>
+        <div className="mt-6 p-6 flex flex-col gap-3">
+          <p className="text-xl font-semibold">Comments</p>
+          <Textarea placeholder="Add a comment" />
         </div>
       </Card>
     </div>
